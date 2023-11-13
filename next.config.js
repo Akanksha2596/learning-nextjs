2515a815f97dcda9c7550cbbf6e8957d501361ea
Akanksha2596 <akanksha.yadav@brainvire.com>
2023-11-13 22:19:24 +0530

    redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async()=>{
+    return [
+      {
+      source : "/about",
+      destination: "/",
+      // permanent : false, //staus code 307 temporary redirect --status code imp for crawlers and search engines
+       permanent : true, //staus code 308 permanent redirect
+
+    },
+  ]
+  }
 }
 
 module.exports = nextConfig
